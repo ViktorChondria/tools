8a4b1aa89bc65cb816ce0078d2eb04efff6e44a0
pandaxtc <22638037+pandaxtc@users.noreply.github.com>
2018-12-03 17:24:31 -0800

    Fix line endings and offset

--- archiver.py
+++ archiver.py
@@ -8,66 +8,70 @@ format: signature (6 bytes), number of elements (4 bytes), [md5 hash of filename
 import glob, os, sys, struct, argparse, hashlib
 
 def generate_archive(archive_name, files):
-	signature = [0x76, 0x69, 0x6b, 0x74, 0x6f, 0x72]
-	nulls = [00, 00, 00, 00, 00, 00, 00, 00, 00, 00]
+    signature = [0x76, 0x69, 0x6b, 0x74, 0x6f, 0x72]
+    nulls = [00, 00, 00, 00, 00, 00, 00, 00, 00, 00]
 
-	header = signature
-	# number of files (and ToC chunks)
-	header.extend(((32 - len(files).bit_length()) // 8) * [00] + [len(files)])
+    header = signature
 
-	current_offset = 10
+    # number of files (and ToC chunks)
+    header.extend([len(files)] + ((32 - len(files).bit_length()) // 8) * [00])
+    current_offset = 10
 
-	# generate header
-	for file in files:
-		m = hashlib.md5()
-		m.update(os.path.basename(file.name).encode('utf-8'))
-		h = m.digest()
+    # generate header
+    for file in files:
+        m = hashlib.md5()
+        m.update(os.path.basename(file.name).encode('utf-8'))
+        h = m.digest()
 
-		# filename hash
-		header.extend(h)
-		
-		# file offset
-		header.extend(((32 - current_offset.bit_length()) // 8) * [00] + [current_offset])
-		current_offset += file.tell() + 10
+        # filename hash
+        header.extend(h)
+        
+        # file offset
+        header.extend(current_offset.to_bytes(4, byteorder='little'))
+        file.seek(0, 2)
+        current_offset += file.tell() + 10
+        file.seek(0)
 
-	archive = open(archive_name, 'wb')
-	# write header
-	archive.write(bytearray(header))
+    archive = open(archive_name, 'wb')
+    # write header
+    print(header)
+    archive.write(bytearray(header))
 
-	for file in files:
-		# padding
-		archive.write(bytearray(nulls))
-
-		# file content
-		chunk = file.read(1024)
-		while(chunk):
-			archive.write(chunk)
-			chunk = file.read(1024)
-
-		print(f'Archived file {file.name} checksum {m.hexdigest()} size {file.tell()}')
-
-	return archive
+    for file in files:
+        # padding
+        archive.write(bytearray(nulls))
+        
+        # file content
+        chunk = file.read(1024)
+        
+        while(chunk):
+            archive.write(chunk)
+            chunk = file.read(1024)
+        
+        print(f'Archived file {file.name} checksum {m.hexdigest()} size {file.tell()}')
+    
+    return archive
 
 def main():
-	parser = argparse.ArgumentParser(description='vn archive generator')
-	parser.add_argument('input_dir', action='store')
-	parser.add_argument('-o', action='store', dest='out', default=None)
-
-	args = parser.parse_args()
-	if args.out == None:
-		args.out = args.input_dir
-	args.out += '.arc'
-	print(f'Archiving {args.input_dir} into {args.out}')
-
-	files = []
-	os.chdir(args.input_dir)
-	for file in glob.glob('*'):
-		files.append(open(file, mode='rb'))
-
-	os.chdir('../')
-	generate_archive(args.out, files)
-
-	print('Done!')
+    parser = argparse.ArgumentParser(description='vn archive generator')
+    parser.add_argument('input_dir', action='store')
+    parser.add_argument('-o', action='store', dest='out', default=None)
+    
+    args = parser.parse_args()
+    if args.out == None:
+        args.out = args.input_dir
+    args.out += '.arc'
+    print(f'Archiving {args.input_dir} into {args.out}')
+    
+    files = []
+    os.chdir(args.input_dir)
+    for file in glob.glob('*'):
+        files.append(open(file, mode='rb'))
+    
+    os.chdir('../')
+    generate_archive(args.out, files)
+    
+    print('Done!')
 
 if __name__=='__main__':
-	main()
+    main()
